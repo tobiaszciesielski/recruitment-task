88cebf476365d65b14916b706dfed53ca70bc4d8
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-05-10 14:56:18 +0200

    build command changed

---
+++ backend/build/jest.config.d.ts
@@ -0,0 +1,4 @@
+declare const _default: {
+    testEnvironment: string;
+};
+export default _default;
---
+++ backend/build/src/utils/encryption.d.ts
@@ -0,0 +1,4 @@
+export declare function encode(mess: string, key?: string): string;
+export declare function decode(encoded: string, key?: string): string;
+export declare function createToken(id: string): string;
+export declare function decodeToken(token: string): string;
