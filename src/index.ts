9a508285ffa7f3c5639697523bbc125126e684ed
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-05-10 15:44:20 +0200

    Removed rocked

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import helmet from "helmet";
 import "./db/index";
 import apiRouter from "./routes/api";
 import { port } from "../src/config";
-import LogRocket from "logrocket";
-LogRocket.init("57idnn/recruitment-task-tc");
 
 const app = express();
 app.use(helmet());
