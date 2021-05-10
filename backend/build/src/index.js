cc9a37ac616a58cfd88e16dd505fe4065f64a398
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-05-10 15:52:29 +0200

    Changed build content

--- backend/build/src/index.js
+++ backend/build/src/index.js
@@ -9,8 +9,6 @@ const helmet_1 = __importDefault(require("helmet"));
 require("./db/index");
 const api_1 = __importDefault(require("./routes/api"));
 const config_1 = require("../src/config");
-const logrocket_1 = __importDefault(require("logrocket"));
-logrocket_1.default.init("57idnn/recruitment-task-tc");
 const app = express_1.default();
 app.use(helmet_1.default());
 app.use(cors_1.default());
